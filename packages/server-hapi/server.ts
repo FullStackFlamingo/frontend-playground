93092d99c56ecc54a445eab335592056a080dd5e
FullStackFlamingo <15013109+FullStackFlamingo@users.noreply.github.com>
2022-08-06 18:33:49 +0100

    fix: add hapi plugin blipp

--- packages/server-hapi/server.ts
+++ packages/server-hapi/server.ts
@@ -1,6 +1,7 @@
 import * as Hapi from '@hapi/hapi';
 import * as Inert from '@hapi/inert';
 import * as H2o2 from '@hapi/h2o2';
+import * as Blipp from 'blipp';
 import { routes } from './routes.js';
 const init = async () => {
   const server: Hapi.Server = Hapi.server({
@@ -10,6 +11,7 @@ const init = async () => {
 
   await server.register(Inert);
   await server.register(H2o2);
+  await server.register(Blipp);
   server.route(routes);
   await server.start();
   console.log('info', { msg: 'Server running on %s', uri: server.info.uri });
