fabf44bda3ef40afbcc45071f9973c1473dcc58c
FullStackFlamingo <15013109+FullStackFlamingo@users.noreply.github.com>
2022-08-27 20:30:17 +0100

    fix(vue-ui): skip to content

---
+++ packages/vue-ui/src/components/SkipToContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <a class="skip-to-content-link" href="#main-content">Skip to content</a>
+</template>
+
+<style scoped>
+.skip-to-content-link {
+  background: #fff;
+  color: #000;
+  top: 40px;
+  left: 40px;
+  padding: 20px;
+  position: fixed;
+  transform: translateY(-1000px);
+}
+
+.skip-to-content-link:focus {
+  transform: translateY(0);
+}
+</style>
