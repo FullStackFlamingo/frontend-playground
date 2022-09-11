58de91c64d207ff9a4a75fff24a785ca0131eea0
FullStackFlamingo <15013109+FullStackFlamingo@users.noreply.github.com>
2022-09-11 20:56:06 +0100

    fix(server-hapi): SSR fixed

--- packages/vue-ui/vite.config.ssr.js
+++ packages/vue-ui/vite.config.ssr.js
@@ -4,10 +4,16 @@ import baseConfig from './vite.config';
 // https://vitejs.dev/config/
 export default defineConfig({
   ...baseConfig,
+  ssr: {
+    format: 'cjs',
+  },
   build: {
     ssr: true,
     rollupOptions: {
       input: './src/entry-server.js',
+      output: {
+        entryFileNames: 'entry-server.js',
+      },
     },
     outDir: './dist/server',
   },
